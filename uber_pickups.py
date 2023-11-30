e4ea567be186383ec6166b08b03f7626c0d406b9
ElenaGrafUZH <91155540+ElenaGrafUZH@users.noreply.github.com>
2023-11-30 10:55:57 +0100

    Add files via upload

---
+++ uber_pickups.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+#load data
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x : str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text('Done! (using st.cache_data)')
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins= 24, range=(0,24))[0]
+
+st.bar_chart(hist_values)
+
+hour_to_filter = st.selectbox('hour', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader('Map of all pickups')
+st.map(filtered_data)
